1cf1ef844e2b5ffe068f60240eb4e7f40d3b5c89
kornelijepetak <kornelije@gmail.com>
2015-10-21 20:05:20 +0200

    Add Dispatcher

---
+++ Blog/Scripts/D.js
@@ -0,0 +1,30 @@
+﻿
+var D = (function() {
+
+	var handlers = {};
+	var token = 0;
+
+	return {
+
+		publish: function(key, data) {
+			if (handlers[key] !== undefined)
+				for (var handlerToken in handlers[key])
+					handlers[key][handlerToken](data);
+		},
+
+		subscribe: function(key, callback) {
+
+			if (handlers[key] === undefined)
+				handlers[key] = {};
+
+			var thisToken = token++;
+			handlers[key][thisToken] = callback;
+			return thisToken;
+		},
+
+		unsubscribe: function(key, token) {
+			if (handlers[key] !== undefined)
+				delete handlers[key][token];
+		}
+	};
+})();
